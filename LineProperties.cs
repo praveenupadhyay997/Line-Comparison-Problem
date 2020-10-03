db9fbf0d5219587d7569b80e9eec0a3a48d7d151
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-03 22:53:29 +0530

    [Praveen] Add: Welcome message for Line Comparison Problem

---
+++ LineProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonProblem
+{
+    class LineProperties
+    {
+        public double absiccaFirstPoint;
+        public double ordinateFirstPoint;
+        public double absiccaSecondPoint;
+        public double ordinateSecondPoint;
+
+        public LineProperties(double absiccaFirstPoint, double ordinateFirstPoint, double absiccaSecondPoint, double ordinateSecondPoint)
+        {
+            this.absiccaFirstPoint = absiccaFirstPoint;
+            this.absiccaSecondPoint = absiccaSecondPoint;
+            this.ordinateFirstPoint = ordinateFirstPoint;
+            this.ordinateSecondPoint = ordinateSecondPoint;
+        }
+
+     
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============================================");
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("==============================================");
+
+        }
+    }
+}
